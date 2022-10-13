5952161f67025dd57088034510c32794cbe172c8
aluno <aluno@udicentro.local>
2022-10-13 20:51:23 -0300

    projeto 2 aula 13/10

---
+++ Pratica02_Synchronized/src/Synchronized_1.java
@@ -0,0 +1,37 @@
+
+public class Synchronized_1 {
+	static int i = -1;
+	
+	
+	public static void main(String[] args) {
+		MeuRunnable runnable = new MeuRunnable();
+
+				Thread t0 = new Thread(runnable);
+				Thread t1 = new Thread(runnable);
+				Thread t2 = new Thread(runnable);
+				Thread t3 = new Thread(runnable);
+				Thread t4 = new Thread(runnable);
+				
+				t0.start();
+				t1.start();
+				t2.start();
+				t3.start();
+				t4.start();
+	}
+	
+	public class MeuRunnable implements Runnable {
+
+		@Override
+		public void run() {
+
+		i++;
+		String name = Thread.currentThread().getName();
+
+		System.out.println(name + ": " + i);
+
+		}		
+	}
+	
+}
+
+
